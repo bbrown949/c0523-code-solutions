552aa20ade57ee8b2e4492eceab239f9211f6f3e
Bryan Brown <bryanbrown949@gmail.com>
2023-06-28 13:07:27 +0000

    completed react-validated-input-component

---
+++ react-validated-input-component/my-app/src/ValidatedInput.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export default function ValidatedInput() {
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  let text = '';
+  let checkOrX = '';
+
+  if (password.length >= 0 && password.length < 8) {
+    text = 'Your password is too short.';
+    checkOrX = '❌';
+  }
+  if (password.length === 0) {
+    text = 'A password is required.';
+    checkOrX = '❌';
+  } else if (password.length > 7) {
+    checkOrX = '✅';
+  }
+
+  return (
+    <>
+      <label>Password</label>
+      <div>
+        <input
+          type="password"
+          name="password"
+          onChange={(event) => setPassword(event.target.value)}
+          value={password}
+          onSubmit={handleSubmit}></input>
+        <span class="checkOrX">{checkOrX}</span>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
