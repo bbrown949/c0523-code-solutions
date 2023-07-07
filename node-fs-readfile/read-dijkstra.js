0c4f77dd482b08b08007fdc3c8cbfc28a36c781b
Bryan Brown <bryanbrown949@gmail.com>
2023-07-07 13:36:25 +0000

    completed node-fs-readfile

---
+++ node-fs-readfile/read-dijkstra.js
@@ -0,0 +1,8 @@
+import { readFile } from 'node:fs/promises';
+
+try {
+  const text = await readFile('dijkstra.txt', 'utf8');
+  console.log(text);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
